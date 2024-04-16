4e79ce75c7bf4d3a06895672f3040425c60cf768
Debiason <debiasonw@gmail.com>
2024-04-16 20:43:01 -0300

    second day

--- index.js
+++ index.js
@@ -1,32 +1,43 @@
 const form = document.getElementById("form");
 form.addEventListener("submit", logSubmit);
 
+var contador = document.getElementById('contadorCaracteres');
+contador.textContent = '0/120';
+
 function logSubmit(event) {
     event.preventDefault();
     const nome = document.getElementById('name').value
     const data = document.getElementById('data_nascimento').value
     var lista = [];
     var logDiv = document.getElementById('log');
+    var spanNome = document.getElementById('msgNome');
     logDiv.innerHTML = '';
 
     if (nome.trim() !== '' && data.trim() !== '') {
-        lista.push({ nome: nome, data: data });
-        var html = '<ul>';
-        lista.forEach(function (item) {
-            var dataFormatada = formatarData(item.data);
-            html += '<li>Nome: ' + item.nome + '</li>';
-            html += '<li>Data: ' + dataFormatada + '</li>';
-        });
-
-        html += '</ul>';
-
-
-        console.log(item.nome);
-        console.log(dataFormatada);
-
-        logDiv.innerHTML = html;
-        document.getElementById('name').value = '';
-        document.getElementById('data_nascimento').value = '';
+        if (nome.length < 3 || nome.length > 120) {
+            spanNome.innerHTML = 'O nome deve ter entre 3 e 120 caracteres';
+            setInterval(() => {
+                spanNome.innerHTML = '';
+            }, 3000);
+        } else {
+    
+            lista.push({ nome: nome, data: data });
+            var html = '<ul>';
+            lista.forEach(function (item) {
+                var dataFormatada = formatarData(item.data);
+                html += '<li>Nome: ' + item.nome + '</li>';
+                html += '<li>Data: ' + dataFormatada + '</li>';
+                console.log(item.nome);
+                console.log(dataFormatada);
+            });
+            
+            html += '</ul>';
+            
+            logDiv.innerHTML = html;
+            document.getElementById('name').value = '';
+            document.getElementById('data_nascimento').value = '';
+            contador.textContent = '0/120';
+        }
     } else {
         logDiv.innerHTML = '<span class ="text-danger">Revise os dados digitados</span>'
     }
@@ -40,4 +51,13 @@ function formatarData(data) {
 
     return dia + '/' + mes + '/' + ano;
 }
+
+document.getElementById('name').addEventListener('input', function() {
+    var contador = document.getElementById('contadorCaracteres');
+    var nomeInput = document.getElementById('name');
+    var nome = nomeInput.value;
+
+    contador.textContent = nome.length + '/120';
+});
+
 
