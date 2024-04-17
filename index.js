547f46947c7bbba8ef56a74cb3a145791dc30cf9
Debiason <debiasonw@gmail.com>
2024-04-16 21:52:01 -0300

    third day

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
+const logDiv = document.getElementById('log');
 const form = document.getElementById("form");
 form.addEventListener("submit", logSubmit);
 
 var contador = document.getElementById('contadorCaracteres');
 contador.textContent = '0/120';
 
+document.addEventListener("DOMContentLoaded", recuperarDadosFormulario);
+
 function logSubmit(event) {
     event.preventDefault();
+    const logDiv = document.getElementById('log');
     const nome = document.getElementById('name').value
     const data = document.getElementById('data_nascimento').value
-    var lista = [];
-    var logDiv = document.getElementById('log');
     var spanNome = document.getElementById('msgNome');
     logDiv.innerHTML = '';
 
@@ -20,23 +22,7 @@ function logSubmit(event) {
                 spanNome.innerHTML = '';
             }, 3000);
         } else {
-    
-            lista.push({ nome: nome, data: data });
-            var html = '<ul>';
-            lista.forEach(function (item) {
-                var dataFormatada = formatarData(item.data);
-                html += '<li>Nome: ' + item.nome + '</li>';
-                html += '<li>Data: ' + dataFormatada + '</li>';
-                console.log(item.nome);
-                console.log(dataFormatada);
-            });
-            
-            html += '</ul>';
-            
-            logDiv.innerHTML = html;
-            document.getElementById('name').value = '';
-            document.getElementById('data_nascimento').value = '';
-            contador.textContent = '0/120';
+            salvarDadosFormulario(nome, data);
         }
     } else {
         logDiv.innerHTML = '<span class ="text-danger">Revise os dados digitados</span>'
@@ -52,12 +38,55 @@ function formatarData(data) {
     return dia + '/' + mes + '/' + ano;
 }
 
-document.getElementById('name').addEventListener('input', function() {
+document.getElementById('name').addEventListener('input', function () {
     var contador = document.getElementById('contadorCaracteres');
     var nomeInput = document.getElementById('name');
     var nome = nomeInput.value;
 
     contador.textContent = nome.length + '/120';
 });
+
+function salvarDadosFormulario(nome, data) {
+    const pessoas = JSON.parse(localStorage.getItem('pessoas')) || [];
+    pessoas.push({ nome: nome, data: data });
+    localStorage.setItem('pessoas', JSON.stringify(pessoas));
+
+    adicionaPessoaTabela(pessoas);
+
+}
+
+function adicionaPessoaTabela(pessoas) {
+    const logDiv = document.getElementById('log');
+    var html = `<table class="table table-striped">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Nome</th>
+        <th scope="col">Data Nascimento</th>
+      </tr>
+    </thead>
+    <tbody>`;
+    i=0;
+    pessoas.forEach(function (item) {
+        var dataFormatada = formatarData(item.data);
+        html += `<tr>
+        <th scope="row">${i}</th>
+        <td>${item.nome}</td>
+        <td>${item.data}</td>
+      </tr>`;
+      i +=1;
+    });
+    html += '</table>';
+
+    logDiv.innerHTML = html;
+    document.getElementById('name').value = '';
+    document.getElementById('data_nascimento').value = '';
+    contador.textContent = '0/120';
+}
+
+function recuperarDadosFormulario() {
+    const pessoas = JSON.parse(localStorage.getItem('pessoas')) || [];
+    adicionaPessoaTabela(pessoas);
+}
 
 
